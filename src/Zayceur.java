f5d030c4c6691f970e1806ac6ea6964490e60775
eleve <coquelle.gael@gmail.com>
2022-03-29 15:33:47 +0200

    v2

--- src/Zayceur.java
+++ src/Zayceur.java
@@ -4,7 +4,7 @@ import java.util.stream.Collectors;
 
 public class Zayceur {
 
-    String[][] plateau;
+    String[][] salle;
     int tours;
     int n;
     int nbrinfecte;
@@ -241,7 +241,7 @@ public class Zayceur {
         }
     };
 
-        public void init(){
+        public void init(){//initialise l'application
             tours =0;
             nbrinfecte=0;
             infecte.clear();
@@ -253,47 +253,50 @@ public class Zayceur {
                 System.out.println("le plateau n'est pas valide");
                 n=Fonction.saisirInt();
             }
-            plateau = new String[n][n];
-            for (int i=0; i<plateau.length;i++){
-                for (int j=0; j<plateau.length;j++){
-                        plateau[i][j]="'";
+            salle = new String[n][n];
+            for (int i=0; i<salle.length;i++){
+                for (int j=0; j<salle.length;j++){
+                        salle[i][j]="'";
                 }
             }
         }
 
-        public void initCoordonnées(){
+        public void initCoordonnées(){ //Code Principal
             int y = 0,x=0;
             boolean debut=false;
             boolean fin=false;
             init();
             System.out.println("Saisisez La Position du/des patient 0");
-            sortirPlateau(this.plateau);
+            sortirPlateau(this.salle);
             while (debut==false){
-                System.out.println("Veuillez Saisir x (entre 0 et "+ (plateau.length-1)+")");
-                x=saisirXY(plateau);
-                System.out.println("Veuillez Saisir y (entre 0 et "+ (plateau.length-1)+")");
-                y=saisirXY(plateau);
-                if (plateau[y][x].equals("*")){
+                System.out.println("Veuillez Saisir x (entre 0 et "+ (salle.length-1)+")");
+                x=saisirXY(salle);
+                System.out.println("Veuillez Saisir y (entre 0 et "+ (salle.length-1)+")");
+                y=saisirXY(salle);
+                if (salle[y][x].equals("*")){
                     System.out.println("Il est deja infecté");
                 }else{
-                    assigner(plateau,x,y);
+                    assigner(salle,x,y);
                 ints.add(new int[]{x, y});
                 nbrinfecte++;
                 }
-                sortirPlateau(plateau);
+                sortirPlateau(salle);
                 debut=stop();
             }
             System.out.println("Etape 0");
-            sortirPlateau(this.plateau);
+            sortirPlateau(this.salle);
             infecte.add(nbrinfecte);
             while(fin==false){
                 tours++;
-                générationSuivante(plateau);
+                générationSuivante(salle);
                 System.out.println("Etape "+ tours);
-                sortirPlateau(this.plateau);
+                sortirPlateau(this.salle);
                 fin=arret();
             }
-            System.out.println("Resultat : "+infecte.toArray());
+            System.out.print("Resultat : ");
+            for (int intValue : infecte) {
+                System.out.println(intValue);
+            }
         }
 
         public static List<Integer> virus(List<int[]> patients0, int n){
@@ -302,7 +305,7 @@ public class Zayceur {
 
 
 
-        public void sortirPlateau(String[][] plateau) {
+        public void sortirPlateau(String[][] plateau) { //afficher le plateau
             for (int i=0; i< plateau.length;i++){
                 for (int j=0; j<plateau.length;j++){
                     System.out.print(plateau[i][j]);
@@ -310,13 +313,13 @@ public class Zayceur {
                 System.out.println();
             }
         }
-        public void voisin(String[][] plateau, Integer x, Integer y){
+        public void voisin(String[][] plateau, Integer x, Integer y){ //modifier les voisin de la valeur
             if (x==0){
                 if (y==0){
                     assigner(plateau,x+1,y);
                     assigner(plateau,x,y+1);
                 }
-                else if(y==plateau.length){
+                else if(y==(plateau.length-1)){
                     assigner(plateau,x+1,y);
                     assigner(plateau,x,y-1);
                 }
@@ -326,12 +329,12 @@ public class Zayceur {
                     assigner(plateau,x,y-1);
                 }
             }
-            else if (x==plateau.length){
+            else if (x==(plateau.length-1)){
                 if (y==0){
                     assigner(plateau,x-1,y);
                     assigner(plateau,x,y+1);
                 }
-                else if (y==plateau.length){
+                else if (y==(plateau.length-1)){
                     assigner(plateau,x-1,y);
                     assigner(plateau,x,y-1);
                 }
@@ -342,24 +345,37 @@ public class Zayceur {
                 }
             }
             else{
-                assigner(plateau,x,y-1);
-                assigner(plateau,x,y+1);
-                assigner(plateau,x-1,y);
-                assigner(plateau,x+1,y);
+                if (y==0){
+                    assigner(plateau,x-1,y);
+                    assigner(plateau,x+1,y);
+                    assigner(plateau,x,y+1);
+                }
+                else if(y==(plateau.length-1)){
+                    System.out.println("g");
+                    assigner(plateau,x-1,y);
+                    assigner(plateau,x+1,y);
+                    assigner(plateau,x,y-1);
+                }
+                else{
+                    System.out.println("h");
+                    assigner(plateau,x,y-1);
+                    assigner(plateau,x,y+1);
+                    assigner(plateau,x-1,y);
+                    assigner(plateau,x+1,y);
+                }
             }
         }
 
-        public void assigner(String[][] plateau, Integer x, Integer y){
-                plateau[y][x]="*";
+        public void assigner(String[][] plateau, Integer x, Integer y){//assigner une valeur a l'endroit voulue
+                plateau[x][y]="*";
         }
 
-        public void générationSuivante(String[][] plateau){
+        public void générationSuivante(String[][] plateau){ //permet de passez d'une etape a une autre //fonction restant a modifiée
             String [][] temp = plateau;
-            sortirPlateau(temp);
-            for (int i=0;i<temp.length;i++){
-                for (int j=0;j<temp.length;j++){
+            for (int i=0;i<plateau.length;i++){
+                for (int j=0;j<plateau.length;j++){
                     if (temp[i][j].equals("*")){
-                        voisin(plateau,i,j);
+                        voisin(plateau,i,j);// tous ce qui est infèrieux est modifièe
                         ints.add(new int[]{i, j});
                         nbrinfecte++;
                     }
@@ -368,7 +384,7 @@ public class Zayceur {
             infecte.add(nbrinfecte);
         }
 
-        public Integer saisirXY(String[][] damier) {
+        public Integer saisirXY(String[][] damier) {//saisir la valeur
             Scanner input = new Scanner(System.in);
             Integer i;
             try {
@@ -402,29 +418,29 @@ public class Zayceur {
             }
             return finInit;
         }
-
+        //fonction pour arreter le code de se lancer jusqu'a qu'une chaine de caractère soit envoyé
         public boolean arret(){
             boolean boo = true;
             Scanner input = new Scanner(System.in);
             String fin;
             System.out.println("");
             fin = input.next();
-            for (int a=0; a<plateau.length; a++){
-                for (int b=0; b<plateau.length; b++){
-                    if (plateau[a][b].equals("'")) {
+            for (int a=0; a<salle.length; a++){
+                for (int b=0; b<salle.length; b++){
+                    if (salle[b][a].equals("'")) {
                         boo=false;
                     }
                 }
             }return boo;
         }
 
-        public static void main(String[] args) throws InterruptedException {
+        public static void main(String[] args) throws InterruptedException {//lancer l'application
             afficheTitre();
             Zayceur jeu=new Zayceur();
             jeu.initCoordonnées();
         }
 
-        private static void afficheTitre() {
+        private static void afficheTitre() {//afficher le titre en tag
             // http://patorjk.com/software/taag/
 
             System.out.println(
